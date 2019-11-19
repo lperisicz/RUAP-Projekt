d6e74215126206422cce68273803810e6bc8a4b8
Luka Perisic <luka.perisic@gauss.hr>
2019-11-19 12:08:53 +0100

    cherry picked to old msg without index number

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -6,7 +6,11 @@ namespace Projekt
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Luka Perišić-Nemam Index");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od Luka Perišić");
+>>>>>>> 957c258... Merge branch 'development' of https://github.com/lperisicz/RUAP-Projekt
         }
     }
 }
